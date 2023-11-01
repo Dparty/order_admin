baa1d039ed4303d24c517fc405b76ee6a3f1d476
Jaythan <jaythan@JaythandeMacBook-Pro.local>
2023-11-01 11:33:43 +0800

    feat: highlight tables have orders

--- lib/models/bill.dart
+++ lib/models/bill.dart
@@ -5,16 +5,19 @@ class Bill {
   final String status;
   final int pickUpCode;
   final List<Order> orders;
+  final String? TableLabel;
   const Bill(
       {required this.id,
       required this.status,
       required this.orders,
-      required this.pickUpCode});
+      required this.pickUpCode,
+      this.TableLabel});
   factory Bill.fromJson(Map<String, dynamic> json) {
     return Bill(
         id: json['id'],
         status: json['status'],
         pickUpCode: json['pickUpCode'],
+        TableLabel: json['TableLabel'],
         orders: (json['orders'] as Iterable)
             .map((e) => Order.fromJson(e))
             .toList());
